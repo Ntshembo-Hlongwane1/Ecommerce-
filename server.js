aed50eef9e48b064d9573f6f4389c6d10a2fdfd7
Ntshembo <juniorWebProjects@gmail.com>
2020-11-27 23:31:20 +0200

    Add: Setup server basic boilerplate code

---
+++ server.js
@@ -0,0 +1,62 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import expressSession from "express-session";
+import MongoStore from "connect-mongodb-session";
+dotenv.config();
+
+const app = express();
+
+//======================================================Middlewares=====================================================
+app.use(
+  cors({
+    origin: "http://localhost:3000/",
+    credentials: true,
+  })
+);
+
+const mongoStore = MongoStore(expressSession);
+const mongoURI = process.env.mongoURI;
+
+const store = new mongoStore({
+  collection: "usersessions",
+  uri: mongoURI,
+  expires: 10 * 60 * 60 * 24 * 1000,
+});
+
+app.use(
+  expressSession({
+    name: "_sid",
+    secret: process.env.session_secret,
+    saveUninitialized: false,
+    resave: false,
+    store: store,
+    cookie: {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 10 * 60 * 60 * 24 * 1000,
+      sameSite: false,
+    },
+  })
+);
+
+//==================================================MongoDB Configs & Connection========================================
+const mongoDB__connectionOptions = {
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+mongoose.connect(mongoURI, mongoDB__connectionOptions, (error) => {
+  if (error) {
+    return console.error(error);
+  }
+  console.log("Connection to MongoDB was successful");
+});
+
+//=================================================Server Configs & Connection==========================================
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server started on PORT ${PORT}`);
+});
